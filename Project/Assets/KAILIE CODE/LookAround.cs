1137a9c66ae259990c978d1bc8fdfd93d30adb9f
kweekiwi <61126528+kweekiwi@users.noreply.github.com>
2022-09-20 02:55:06 -0500

    Added movement code
    Also removed the asylum assets bc they big time crashed my unity a few times. They were my assets so I can put them back in but I don't think we need them.

---
+++ Project/Assets/KAILIE CODE/LookAround.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * (Kailie Otto)
+ * (Group project 1)
+ * (Allows player to look around. Followed tutorial and edited to better fit game
+ * http://gyanendushekhar.com/2020/02/06/first-person-movement-in-unity-3d/)
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    // horizontal rotation speed
+    public float horizontalSpeed = 2f;
+    // vertical rotation speed
+    public float verticalSpeed = 2f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+
+        //clamps rotation
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+    }
+}
