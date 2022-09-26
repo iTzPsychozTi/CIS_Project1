c0033c7e90a9f16f4a08cee4335e042e2278080e
kweekiwi <61126528+kweekiwi@users.noreply.github.com>
2022-09-26 17:06:53 -0500

    codeadded

---
+++ Project/Assets/KAILIE CODE/MouseClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    bool closeEnough;
+    public GameObject radius;
+    private trigger check;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        closeEnough = false;
+        check = radius.GetComponent<trigger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (check.triggered)
+        {
+            Debug.Log("mouse click able to happen");
+            closeEnough = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // Destroy the gameObject after clicking on it
+        if (closeEnough)
+        {
+            Debug.Log("clicked");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Project/Assets/KAILIE CODE/trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    public bool triggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        Debug.Log("entered target area");
+    }
+
+}
