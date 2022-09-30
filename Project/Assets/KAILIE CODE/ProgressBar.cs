8d0498c14cbc27f1921f2036335d583b83f311d3
Corinne Bond <corinne32300@gmail.com>
2022-09-29 21:27:56 -0500

    progressBar

---
+++ Project/Assets/KAILIE CODE/ProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode] 
+public class ProgressBar : MonoBehaviour
+{
+    public int max;
+    public int min;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getCurrentFill();
+    }
+
+    void getCurrentFill()
+    {
+        float currentOffset = current - min;
+        float maximumOffset = max - min;
+        float fillAmount = (float)current / (float)max;
+        mask.fillAmount = fillAmount;
+
+        fill.color = color;
+    }
+}
