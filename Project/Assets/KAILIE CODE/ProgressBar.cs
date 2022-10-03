29af1e61b87123e4a0f9b3ccebe8ed87c32a3ff5
Corinne Bond <corinne32300@gmail.com>
2022-10-02 21:55:02 -0500

    progress/timer/gameover

--- Project/Assets/KAILIE CODE/ProgressBar.cs
+++ Project/Assets/KAILIE CODE/ProgressBar.cs
@@ -2,41 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//Corinne Bond -> Tracks progress of how many objects have been collected
 
-[ExecuteInEditMode] 
+[ExecuteInEditMode]
 public class ProgressBar : MonoBehaviour
 {
-    public int max;
-    public int min;
-    public int current;
     public Image mask;
     public Image fill;
+
     public Color color;
-    public SpawnManager spawnManager;
-    public int currentObjectCount;
-    public int totalCount;
 
-
+    public int current;
+    public int max;
+    public int min;
     // Start is called before the first frame update
     void Start()
     {
-        spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
-        totalCount = 10;    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentObjectCount = spawnManager.itemTracker;
         getCurrentFill();
     }
 
     void getCurrentFill()
     {
-        //float currentOffset = current - min;
-        //float maximumOffset = max - min;
-        float fillAmount = (float) currentObjectCount / (float) totalCount;
+        float currentOffset = current - min;
+        float maximumOffset = max - min;
+        float fillAmount = (float)current / (float)max; 
         mask.fillAmount = fillAmount;
 
         fill.color = color;
--- Project/Assets/KAILIE CODE/SpawnManager.cs
+++ Project/Assets/KAILIE CODE/SpawnManager.cs
@@ -9,11 +9,13 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public const int N = 9;
-    public GameObject[] itemPrefab;
+    public const int N = 10; // 10 items to be collected
+    public GameObject[] itemPrefab = new GameObject[N];
     public Transform[] spawnLocation;
 
     public int itemTracker;
+
+    private MouseClick mouseClickScript;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +27,20 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         //if the current item has been "found" destroy the current game object,increment the tracker and spawn the next item
-        if(itemTracker <= N)
+        if(true)
         {
+            //Destroy(itemPrefab[itemTracker]);
             itemTracker++;
             spawnTargetItems();
         }
+       
+
     }
     void spawnTargetItems()
     {
+ 
         Instantiate(itemPrefab[itemTracker], spawnLocation[itemTracker].transform.position, Quaternion.Euler(0, 0, 0));
     }
 }
