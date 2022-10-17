517be67970bdd0a64ba32a21d1563de61eb24d5a
iTzPsychozTi <krichardson3@mail.bradley.edu>
2022-10-17 00:41:50 -0500

    Updated UI Management

---
+++ Project/Assets/Scripts/IntrodutionText.cs
@@ -0,0 +1,25 @@
+﻿/* (Kyree Richardson)
+ * (Project 1)
+ * (Displays the objective of the game)
+ * 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntrodutionText : MonoBehaviour
+{
+    public Text introText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //disables text after 10 seconds
+        Invoke("DisableText", 10f);
+    }
+
+   void DisableText()
+    {
+        introText.enabled = false;
+    }
+}
